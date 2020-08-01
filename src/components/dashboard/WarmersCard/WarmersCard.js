6333c16479669559116dcda95d6a4f8c64bd8abf
Ivo Evans <ievans.storrie@gmail.com>
2020-08-01 11:17:57 +0100

    feat(warmers): style pie chart

--- src/components/dashboard/WarmersCard/WarmersCard.js
+++ src/components/dashboard/WarmersCard/WarmersCard.js
@@ -1,14 +1,20 @@
 import React from "react";
 import PropTypes from "prop-types";
 import * as sc from "../styled-components";
-import styled from "styled-components";
 import { PieChart } from "react-minimal-pie-chart";
 import randomColour from "../../../utils/randomColour"
+import styled from "styled-components"
+
+
+const Chart = styled(PieChart)`
+max-height: 270px;
+`
 
 const WarmersCard = ({ title, warmers }) => {
    const pieData = Object.keys(warmers).map(warmer => {
         return {
             title: warmer,
+            key: warmer,
             value: warmers[warmer],
             color: randomColour()
         }
@@ -17,7 +23,21 @@ const WarmersCard = ({ title, warmers }) => {
       <sc.Card>
         <sc.Title>{title}</sc.Title>
         <sc.Contents>
-          <PieChart data={pieData} viewBoxSize={[300, 300]} />
+          <Chart 
+          data={pieData} 
+          radius={10}
+          viewBoxSize={[50, 50]}
+          center={[25, 15]}
+          background={"none"}
+          labelStyle={(index) => ({
+            fontSize: '2.5px',
+          })}
+          label={(e) => {
+              console.log(pieData[e.dataIndex].title)
+              return pieData[e.dataIndex].title
+          }}
+          labelPosition={112}
+          />
         </sc.Contents>
       </sc.Card>
   );
